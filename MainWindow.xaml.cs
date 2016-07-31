514ccf685411a3b58646df7890ed65231d2dfc42
Antonio Donato <donant@gmail.com>
2016-07-31 18:30:04 +0200

    Commit Iniziale

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DarioAuguri
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Dario esecutore = new Dario();
+            esecutore.RiempiLista();
+            string selezionato = esecutore.Seleziona(esecutore.SelectRandom());
+
+            Process proc = new Process();
+            proc.StartInfo.FileName = selezionato;
+            proc.StartInfo.UseShellExecute = true;
+            proc.Start();
+
+            //SoundPlayer darioVoice = new SoundPlayer(selezionato);
+            //darioVoice.Play();
+        }
+    }
+}
---
+++ random.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DarioAuguri
+{
+    public class Dario
+    {
+        public List<String> _randomList = new List<string>();
+        public string _path;
+        static Random rnd = new Random();
+        private string _searchPattern;
+
+        public static Random Rnd
+        {
+            get { return rnd; }
+            set { rnd = value; }
+        }
+
+        public Dario()
+        {
+        }
+
+        public int SelectRandom()
+        {
+            int r = Rnd.Next(_randomList.Count);
+            return r;
+        }
+
+        public void RiempiLista()
+        {
+            _path = "Resources/";
+            _searchPattern = "*.aac";
+            string[] fileEntries = Directory.GetFiles(_path, _searchPattern);
+
+            foreach (string fileName in fileEntries)
+            {
+                _randomList.Add(fileName);
+            }
+
+        }
+
+        public string Seleziona(int numeroRnd)
+        {
+            return _randomList[numeroRnd];
+        }
+    }
+
+}
